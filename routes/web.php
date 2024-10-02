10235f578aa80d58813a976ef347ffc78ce4e4d8
chenaoh <cdhenao@gmail.com>
2024-10-01 22:13:33 -0600

    vistas

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('activos/show', function () {
+    return view('activos/show');
+});
+Route::get('activos/create', function () {
+    return view('activos/create');
+});
+Route::get('activos/update', function () {
+    return view('activos/update');
 });
